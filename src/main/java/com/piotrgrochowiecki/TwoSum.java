437142bf91719d847e0a2b76f5ad79d6ac48e404
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2022-11-18 18:59:04 +0100

    Two sum:
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
             You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
             You can return the answer in any order.
    
             Example 1:
    
             Input: nums = [2,7,11,15], target = 9
             Output: [0,1]
             Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
    leetcode.com

---
+++ src/main/java/com/piotrgrochowiecki/TwoSum.java
@@ -0,0 +1,16 @@
+package com.piotrgrochowiecki;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    int[] result = {i, j};
+                    return result;
+                }
+            }
+        }
+        int[] arr = {0, 0};
+        return arr;
+    }
+}
